5148c06b5f22710e5a67e0001fdb7a9363617c7e
codingnoobno1 <164862821+codingnoobno1@users.noreply.github.com>
2025-04-28 23:17:39 +0530

    Batch 3: Update MainContent component structure

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,7 +3,7 @@
 import QuizSection from './quiz/QuizSection';
 import CodeEditorSection from './code/CodeEditorSection';
 import ProfileSection from './profile/ProfileSection';
-import ProjectsSection from './projects/ProjectsSection';
+import ProjectsSection from './project/ProjectsSection';
 import SolutionsSection from './solutions/SolutionsSection';
 import PageTitle from './ui/PageTitle';
 
